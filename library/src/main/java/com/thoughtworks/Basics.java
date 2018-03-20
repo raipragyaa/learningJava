ac717244f5b3a6809b3da012e2980291fc0a2c71
pragya <raipragya140@gmail.com>
2018-03-20 17:46:39 +0530

    Added Basics class with very basic methods

---
+++ library/src/main/java/com/thoughtworks/Basics.java
@@ -0,0 +1,39 @@
+package com.thoughtworks;
+
+public class Basics {
+    public boolean isEven(int number) {
+        return number%2==0;
+    }
+
+    public boolean isOdd(int number) {
+        return number%2 != 0;
+    }
+
+    public int square(int num) {
+        return num*num;
+    }
+
+    public int cube(int number) {
+        return number*square(number);
+    }
+
+    public int gcd(int num1, int num2) {
+        if (num1 < num2) {
+            int sum = 0;
+            sum = num1 + num2;
+            num1 = sum - num1;
+            num2 = sum - num1;
+        }
+        int rem = (num1 % num2);
+        while (rem != 0) {
+            num2 = rem;
+            num1 = num2;
+            rem = num1 % num2;
+        }
+        return num2;
+    }
+
+    public int lcm(int num1, int num2) {
+        return (num1 * num2)/gcd(num1, num2);
+    }
+}
---
+++ library/src/test/java/BasicsTest.java
@@ -0,0 +1,49 @@
+import com.thoughtworks.Basics;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class BasicsTest {
+
+    private Basics basics;
+
+    @Before
+    public void setUp(){
+        basics = new Basics();
+    }
+
+    @Test
+    public void isEven(){
+        assertTrue(basics.isEven(4));
+    }
+
+    @Test
+    public  void isOdd(){
+        assertTrue(basics.isOdd(5));
+    }
+
+    @Test
+    public void checkSquare(){
+        assertThat(basics.square(4),is(16));
+    }
+
+    @Test
+    public void checkCube(){
+        assertThat(basics.cube(3),is(27));
+    }
+
+    @Test
+    public void checkGcd(){
+        assertThat(basics.gcd(3,2),is(1));
+    }
+
+    @Test
+    public void checkLcm(){
+        assertThat(basics.lcm(2,3),is(6));
+    }
+    
+}
+
